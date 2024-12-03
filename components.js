57cce59608278b26739db2954b1d592affb7399f
babetCode <adrienbabet1@gmail.com>
2024-12-03 12:45:36 -0700

    css

--- components.js
+++ components.js
@@ -1,12 +1,52 @@
 const components = {
     navbar: `
-        <nav>
-            <ul>
-                <li><a href="index.html">Home</a></li>
-                <li><a href="about.html">About</a></li>
-                <li><a href="contact.html">Contact</a></li>
-            </ul>
-        </nav>
+        <a href="#">
+            <img src="images/logo2-1.png" alt="logo" id="mainheaderlogo">
+        </a>
+        <div id="hamburger">
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+        <div id="dropdown-menu">
+            <a href="html/aboutpage.html">About</a>
+            <a href="html/menupage.html">Menu</a>
+            <a href="html/locationspage.html">Locations</a>
+            <a href="html/employmentpage.html">Employment</a>
+            <a href="html/contactpage.html">Contact</a>
+            <a href="html/storypage.html">Our Story</a>
+        </div>
+        <ul>
+            <li>
+                <a href="html/aboutpage.html">About</a>
+            </li>
+            <li>
+                <a href="html/menupage.html">Menu</a>
+            </li>
+            <li>
+                <a href="html/locationspage.html">Locations</a>
+            </li>
+            <li>
+                <a href="html/employmentpage.html">Employment</a>
+            </li>
+            <li>
+                <a href="html/contactpage.html">Contact</a>
+            </li>
+            <li>
+                <a href="html/storypage.html">Our Story</a>
+            </li>
+        </ul>
+        <div id="socials">
+            <a href="https://www.facebook.com" target="_blank">
+                <img src="images/fb.png" alt="facebook">
+            </a>
+            <a href="https://www.instagram.com" target="_blank">
+                <img src="images/instagram.png" alt="instagram">
+            </a>
+            <a href="https://www.twitter.com" target="_blank">
+                <img src="images/twitterx.png" alt="twitterx">
+            </a>
+        </div>
     `,
     footer: `
         <footer>
