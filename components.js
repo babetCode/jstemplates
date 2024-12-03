a67cc300a875c7e0a3d46bd33dfa126c15bfced7
babetCode <adrienbabet1@gmail.com>
2024-12-03 14:40:12 -0700

    locations page

--- components.js
+++ components.js
@@ -3,19 +3,6 @@ const components = {
         <a href="#">
             <img src="images/logo2-1.png" alt="logo" id="mainheaderlogo">
         </a>
-        <div id="hamburger">
-            <div></div>
-            <div></div>
-            <div></div>
-        </div>
-        <div id="dropdown-menu">
-            <a href="html/aboutpage.html">About</a>
-            <a href="html/menupage.html">Menu</a>
-            <a href="html/locationspage.html">Locations</a>
-            <a href="html/employmentpage.html">Employment</a>
-            <a href="html/contactpage.html">Contact</a>
-            <a href="html/storypage.html">Our Story</a>
-        </div>
         <ul>
             <li>
                 <a href="html/aboutpage.html">About</a>
@@ -52,12 +39,70 @@ const components = {
         <footer>
             <p>© 2024 My Website</p>
         </footer>
+    `,
+    pagenavbar: `
+        <a href="../index.html">
+            <img src="../images/logo2-1.png" alt="logo" id="mainheaderlogo">
+        </a>
+        <ul>
+            <li>
+                <a href="aboutpage.html">About</a>
+            </li>
+            <li>
+                <a href="menupage.html">Menu</a>
+            </li>
+            <li>
+                <a href="locationspage.html">Locations</a>
+            </li>
+            <li>
+                <a href="employmentpage.html">Employment</a>
+            </li>
+            <li>
+                <a href="contactpage.html" class="active">Contact</a>
+            </li>
+            <li>
+                <a href="storypage.html">Our Story</a>
+            </li>
+        </ul>
+        <div id="socials">
+            <a href="https://www.facebook.com" target="_blank">
+                <img src="../images/fb.png" alt="facebook">
+            </a>
+            <a href="https://www.instagram.com" target="_blank">
+                <img src="../images/instagram.png" alt="instagram">
+            </a>
+            <a href="https://www.twitter.com" target="_blank">
+                <img src="../images/twitterx.png" alt="twitterx">
+            </a>
+        </div>
     `
 };
 
+
 function loadComponents() {
-    document.getElementById('navbar').innerHTML = components.navbar;
-    document.getElementById('footer').innerHTML = components.footer;
+    // Check and load the navbar
+    const navbarElement = document.getElementById('navbar');
+    if (navbarElement) {
+        navbarElement.innerHTML = components.navbar;
+    } else {
+        console.warn("Navbar element with id 'navbar' not found.");
+    }
+
+    // Check and load the navbar
+    const pagenavbarElement = document.getElementById('pagenavbar');
+    if (pagenavbarElement) {
+        pagenavbarElement.innerHTML = components.pagenavbar;
+    } else {
+        console.warn("Navbar element with id 'pagenavbar' not found.");
+    }
+
+    // Check and load the footer
+    const footerElement = document.getElementById('footer');
+    if (footerElement) {
+        footerElement.innerHTML = components.footer;
+    } else {
+        console.warn("Footer element with id 'footer' not found.");
+    }
 }
 
 document.addEventListener("DOMContentLoaded", loadComponents);
